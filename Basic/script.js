7f79892e92d36c8fb4449d6bc95846845f7a5e0d
Lidia Cekic <lidiacekic9@gmail.com>
2020-09-21 11:16:17 +0200

    basic one done

---
+++ Basic/script.js
@@ -0,0 +1,9 @@
+var Number = Math.floor(Math.random() * 31) - 5;
+console.log(Number);
+if (Number > 10) {
+  document.write('The weather is moderate');
+} else {
+  document.write('The weather is cold');
+}
+
+// OR: create random number between -5 and -1; and random number between 0 and 25; display randomly between the two numbers
